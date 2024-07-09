928f8cb52dbff3da17b7992d38d8996400681bc0
ricardo.rocha <ricardo.rocha.3devs@fogas.com.br>
2024-07-09 20:49:04 -0300

    Validar campos email, telefone, login e senha

--- src/app/user/user-form/user-form.component.ts
+++ src/app/user/user-form/user-form.component.ts
@@ -98,10 +98,6 @@ export class UserFormComponent implements OnInit {
     });
   }
 
-  logIt(event: any) {
-    console.log(this.userFormGroup.firstName);
-  }
-
   createUser(): any {
     const userToPost = {
       birthday: moment(this.userForm.get('birthday')?.value).format('yy-mm-dd'),
